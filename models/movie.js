d66b9883d6e730688a422008eb30530c064c28c0
void4d <tulubaevp@icloud.com>
2023-10-02 17:53:38 +0500

    add: Добавить модели для фильма и пользователя

---
+++ models/movie.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Поле "Страна" должно быть заполнено']
+  },
+  director: {
+    type: String,
+    required: [true, 'Поле "Режиссёр" должно быть заполнено']
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Поле "Длительность" должно быть заполнено']
+  },
+  year: {
+    type: String,
+    required: [true, 'Поле "Год" должно быть заполнено']
+  },
+  description: {
+    type: String,
+    required: [true, 'Поле "Описание" должно быть заполнено']
+  },
+  image: {
+    type: String,
+    required: [true, 'Поле "Постер" должно быть заполнено']
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Поле "Ссылка на трейлер" должно быть заполнено']
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Поле "Превью" должно быть заполнено']
+  },
+  owner: mongoose.Schema.Types.ObjectId,
+  movieId: {
+    type: Number,
+    required: [true, 'Поле "ID фильма" должно быть заполнено']
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Поле "Название на русском" должно быть заполнено']
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Поле "Название на английском" должно быть заполнено']
+  }
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "Имя" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "Имя" - 2 символа'],
+    maxlength: [30, 'Максимальная длина поля "Имя" - 30 символов'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Некорректный адрес email'
+    }
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "Пароль" должно быть заполнено'],
+    minlength: [6, 'Минимальная длина поля "Пароль" - 6 символов'],
+    select: false
+  }
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
