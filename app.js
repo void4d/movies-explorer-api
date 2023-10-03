742c60ad844018a30228325bd1ca46296dcd350e
void4d <tulubaevp@icloud.com>
2023-10-03 20:18:31 +0500

    add: Добавить логгер

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const userRouter = require('./routes/users');
 const movieRouter = require('./routes/movies');
 const { createUser, login } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger')
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb'} = process.env;
 
@@ -14,12 +15,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 
 app.use(auth);
 app.use(userRouter);
 app.use(movieRouter);
+
+app.use(errorLogger);
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
